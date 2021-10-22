7a1244a8a6236f2a468c17a5f08d25054ed6faa8
nhattien298 <nhattien298@gmail.com>
2021-10-22 20:04:22 +0700

    thêm cái lệnh in hình tam giác cân lên rôi

--- tinhtong.py
+++ tinhtong.py
@@ -1,2 +1,11 @@
 a=int(input('nhập vào một số'))
 print(a)
+
+# in hình tam giác cân
+for row in range(1,5):
+  for col in range(1,8):
+    if row == 4 or row + col == 5 or col - row == 3:
+      print('*',end='')
+    else:
+      print(end=' ')
+  print()
